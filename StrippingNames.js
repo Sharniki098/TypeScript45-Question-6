9f1f25c6740bd8ab1ed0a192ef756debec68a32d
Shumaila Noreen <161513956+Sharniki098@users.noreply.github.com>
2024-05-20 01:16:20 -0700

    Add files via upload

---
+++ StrippingNames.js
@@ -0,0 +1,4 @@
+var personName = "   \n\tMuskan Khan\t\n";
+console.log(personName);
+var without_whitespace = personName.trim();
+console.log(without_whitespace);
---
+++ StrippingNames.ts
@@ -0,0 +1,4 @@
+const personName : string ="   \n\tMuskan Khan\t\n";
+console.log(personName);
+const  without_whitespace: string=personName.trim();
+console.log(without_whitespace);
